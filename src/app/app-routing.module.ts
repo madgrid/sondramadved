50a6207dcdcfb302d3c8133457b40c190d5d5e12
madgrid <aaronmadved@gmail.com>
2018-12-21 15:58:28 -0500

    added routes to newly added pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+import { ArtItem1Component } from './art-item1/art-item1.component';
+import { ArtItem2Component } from './art-item2/art-item2.component';
+import { ArtItem3Component } from './art-item3/art-item3.component';
+import { ArtMoreComponent } from './art-more/art-more.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'art-item1', component: ArtItem1Component },
+  { path: 'art-item2', component: ArtItem2Component },
+  { path: 'art-item3', component: ArtItem3Component },
+  { path: 'art-more', component: ArtMoreComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
