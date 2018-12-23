b2dfe7dd88a1fa1ae7e36efbff0f7a5dad18edd1
madgrid <aaronmadved@gmail.com>
2018-12-23 15:43:12 -0500

    initalize fade animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   title = 'sondramadved';
